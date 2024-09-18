c110fc8cd08ae7abaafa64b4e8c3e4ad34d6bdbd
Daniel Asalde Neyra <yakasaldeneyra@gmail.com>
2024-09-18 10:00:30 -0500

    Add files via upload

---
+++ CLASE (18.09.2024)/script.js
@@ -0,0 +1,32 @@
+console.log("Ejemplos de JavaScript estan en la consola.");
+
+let x = 10;
+let y = 5;
+console.log(x + y);
+
+let suma = x + y;
+let resta = x - y;
+let multiplicacion = x * y;
+let division = x / y;
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multipliacion:", multiplicacion);
+console.log("Division:", division);
+
+let nombre = "Juan";
+let Nombre = "Pedro";
+console.log("Nombre en minuscula:", nombre);
+console.log("Nombre en mayuscula:", Nombre);
+
+let esVerdadero = true;
+let numero = 100;
+let texto = "Hola, mundo";
+console.log("Booleano:", esVerdadero);
+console.log("Numero:", numero);
+console.log("String:", texto);
+
+let numeroEntero = 10;
+let numeroDecimal = 10.5;
+console.log("Numero entero:", numeroEntero);
+console.log("Numero decimal:", numeroDecimal);
+console.log(numeroDecimal*numeroEntero);
